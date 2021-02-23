e49e030b3ef5947bb0221a2a84648e0da1f91165
ashadria <ashadria@gmail.com>
2021-02-23 18:08:44 -0500

    fixed it

--- components/EmployeeSearch.js
+++
@@ -1,7 +0,0 @@
-export default function EmployeeSearch({ handleInput }) {
-    return (
-      <form className="py-1">
-        <input className="form-control" type="search" placeholder="Search" onInput={handleInput} />
-      </form>
-    );
-  }
